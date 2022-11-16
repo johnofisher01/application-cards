190f0805fb4cc06d093c49f5ac5a9ab314cff227
Johno <john@mount-media-online.com>
2022-11-16 09:47:28 +0000

    basic setup components

---
+++ application-cards/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTypes from 'prop-types'
+
+function Header({ text, bgColor,textColor }) {
+
+  const headerStyles ={
+    backgroundColor: bgColor,
+    color:textColor
+  }
+  return (
+    <header style={headerStyles}>
+      <div className="container">
+        <h2>{text}</h2>
+      </div>
+    </header>
+  );
+}
+
+// these will be used automatically if no props passed into <Header/>
+Header.defaultProps = {
+  text: 'Feedback UI',
+  bgColor: 'black',
+  textColor:'#ff6a95'
+}
+
+Header.propTypes = {
+  text: PropTypes.string,
+  bgColor : PropTypes.string,
+  textColor : PropTypes.string
+}
+
+export default Header;
+
+
