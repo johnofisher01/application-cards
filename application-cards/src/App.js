66ca4e238ef199929b2247352f7938ce621539d2
Johno <john@mount-media-online.com>
2022-11-16 11:11:53 +0000

    test push state click

--- application-cards/src/App.js
+++ application-cards/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import './index.css'
 import Header from './components/Header'
+import FeedbackItem from './components/FeedbackItem'
 
 function App() {
 
 
 
   return (
+    <>
       <Header />
+      <div className="container">
+        <FeedbackItem/>
+      </div>
+      </>
   )
 }
 
