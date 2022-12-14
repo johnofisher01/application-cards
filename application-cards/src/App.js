cb6dd8b710390d9f17ef27d62d868cdf62bf5e39
Johno <john@mount-media-online.com>
2022-12-14 21:58:09 +0000

    Card added, reverse style. Default / Proptypes added

--- application-cards/src/App.js
+++ application-cards/src/App.js
@@ -2,8 +2,12 @@ import React from 'react'
 import './index.css'
 import Header from './components/Header'
 import FeedbackItem from './components/FeedbackItem'
+import {useState} from 'react'
+import FeedbackData from './data/data'
+import FeedbackList from './components/FeedbackList'
 
 function App() {
+  const [feedback, setFeedback]=useState(FeedbackData)
 
 
 
@@ -11,7 +15,7 @@ function App() {
     <>
       <Header />
       <div className="container">
-        <FeedbackItem/>
+        <FeedbackList feedback={feedback}/>
       </div>
       </>
   )
--- application-cards/src/components/FeedbackItem.jsx
+++ application-cards/src/components/FeedbackItem.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import { useState } from 'react'
+import Card from './shared/Card'
+import PropTypes from 'prop-types'
 
-function FeedbackItem() {
+function FeedbackItem({item}) {
 
-    const [rating, setRating]=useState(7)
-    const [text, setText]=useState('This is an example of a feedback item')
 
-    const handleClick = () => {
-        setRating((prev)=> {
-            return prev + 1
-        })
-    }
+
 
   return (
-    <div className="card">
-        <div className="num-display">{rating}</div>
+    <Card reverse={true}>
+        <div className="num-display">{item.rating}</div>
         <div className="text-display">
-             {text}
+        {item.text}
+             
         </div>
-        <button onClick={handleClick}>Click</button>
-    </div>
+       
+    </Card>
   )
+}
+
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired
 }
 
 export default FeedbackItem
