1ef0925e464048d02be3a3e90b4297fafe308ac1
Johno <john@mount-media-online.com>
2023-01-18 16:04:45 +0000

    adding stats, basic functions

--- application-cards/src/App.js
+++ application-cards/src/App.js
@@ -5,6 +5,7 @@ import FeedbackItem from './components/FeedbackItem'
 import {useState} from 'react'
 import FeedbackData from './data/data'
 import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
 
 function App() {
   const [feedback, setFeedback]=useState(FeedbackData)
@@ -12,18 +13,14 @@ function App() {
   const deleteFeedback = (id) => {
     if(window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter((item)=> item.id !== id))
-
-    }
-    
-  
+    } 
   }
-
- 
 
   return (
     <>
       <Header />
       <div className="container">
+      <FeedbackStats feedback={feedback}/>
         <FeedbackList handleDelete={deleteFeedback} feedback={feedback}/>
       </div>
       </>
